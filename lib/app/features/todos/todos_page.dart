e48784a1b8d6f901051ff6b618741eaed603a71f
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2021-03-15 21:14:07 -0300

    Fix Todo Page Title

--- lib/app/features/todos/todos_page.dart
+++ lib/app/features/todos/todos_page.dart
@@ -10,7 +10,7 @@ class ToDosPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Relação de TO-Dos'),
+        title: Text('Relação de TO-DOs'),
       ),
       body: SingleChildScrollView(
         child: Column(
